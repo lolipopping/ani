2cf77471a7bc0dbb377f5ce72156017805ec4c42
lolipopping <lolipopping@debian>
2015-10-23 17:06:20 -0700

    added error checking

--- src/anistream.py
+++ src/anistream.py
@@ -6,17 +6,17 @@ from bs4 import BeautifulSoup
 #asks user episode number of series
 #no output
 def stream(scraper, url):
-    #pull from url
-    seriesurl = scraper.get(url)
-
+    try:
+        #pull from url
+        seriesurl = scraper.get(url)
+    except:
+        print("url error")
+        return
     soup = BeautifulSoup(seriesurl.content, "html5lib")
-
     #prints all html
     #print (soup.prettify())
-
     #makes empty array
     linklist = []
-
     i = 0
     #print all links title and add link itself to array
     for link in soup.find_all("a"):
@@ -29,28 +29,25 @@ def stream(scraper, url):
         while True:
             #get episode number
             ep = input("ep :")
-
-            #download episode page
-            epurl = scraper.get("https://kissanime.com" + str(linklist[int(ep)]))
-
+            try:
+                #download episode page
+                epurl = scraper.get("https://kissanime.com" + str(linklist[int(ep)]))
+            except:
+                print("ep number error")
+                return
             soup = BeautifulSoup(epurl.content, "html5lib")
-
             #array to hold videos
             video = []
-
             #get base64 code episodes inside selectQuality and store into array
             for link in soup.find(id="selectQuality").find_all("option"):
                 video.append((link.get("value")))
-
             #select quality
             #q = input("quality :")
-
             #watch highest quality video
             #decode base 64
             play = base64.b64decode(str(video[0]))
             #encode to utf8
             play = play.decode('utf8')
-
             #need quotes to play videos with & in name
             launch = "mpv " + "\"" + play + "\""
             os.system(launch)
